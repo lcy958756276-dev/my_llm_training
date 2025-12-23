258443005ef5fea7137f93cd3498a7f97b7ae389
lcy958756276-dev <lcy958756276@gmail.com>
2025-12-23 17:29:07 +0800

    update code

--- eval/eval.py
+++ eval/eval.py
@@ -1,92 +1,92 @@
-from transformers import AutoTokenizer
-from srctwo.data.utils import test_data,distinguish_eval
-import json
-model_path = r"D:\vscode--llm\my-llm-training\srctwo\models\Qwen2.5-7B"
+# from transformers import AutoTokenizer
+# from srctwo.data.utils import test_data,distinguish_eval
+# import json
+# model_path = r"D:\vscode--llm\my-llm-training\srctwo\models\Qwen2.5-7B"
 
 
-eval_datas=distinguish_eval(test_data)
-print(eval_datas[0])
-true_entities=[]
-true_entities=[json.loads(eval_data['entities']) for eval_data in eval_datas]
-print(true_entities[1])
+# eval_datas=distinguish_eval(test_data)
+# print(eval_datas[0])
+# true_entities=[]
+# true_entities=[json.loads(eval_data['entities']) for eval_data in eval_datas]
+# print(true_entities[1])
 
-from collections import defaultdict
-# true_entities = [
-#     [["口苦","临床表现"]],
-#     [["复方蜥蜴散","方剂"], ["三七","中药"]],
-#     ...
-# ]
+# from collections import defaultdict
+# # true_entities = [
+# #     [["口苦","临床表现"]],
+# #     [["复方蜥蜴散","方剂"], ["三七","中药"]],
+# #     ...
+# # ]
 
-# pred_entities = [
-#     [["口苦","临床表现"]],
-#     [["复方蜥蜴散","方剂"], ["半枝莲","中药"]],
-#     ...
-# ]
+# # pred_entities = [
+# #     [["口苦","临床表现"]],
+# #     [["复方蜥蜴散","方剂"], ["半枝莲","中药"]],
+# #     ...
+# # ]
 
-def compute_f1(true_entities, pred_entities):
-    # 统计 TP, FP, FN
-    tp = defaultdict(int)
-    fp = defaultdict(int)
-    fn = defaultdict(int)
+# def compute_f1(true_entities, pred_entities):
+#     # 统计 TP, FP, FN
+#     tp = defaultdict(int)
+#     fp = defaultdict(int)
+#     fn = defaultdict(int)
 
-    for true, pred in zip(true_entities, pred_entities):
-        #true=[['复方蜥蜴散', '方剂'], ['三七', '中药']]  pred=[['复方蜥蜴散', '方剂'], ['半枝莲', '中药']]
-        true_set = {(t, c) for t, c in true}
-        #{('复方蜥蜴散', '方剂'), ('三七', '中药')}
-        pred_set = {(t, c) for t, c in pred}
-        #转成集合才能求交集
+#     for true, pred in zip(true_entities, pred_entities):
+#         #true=[['复方蜥蜴散', '方剂'], ['三七', '中药']]  pred=[['复方蜥蜴散', '方剂'], ['半枝莲', '中药']]
+#         true_set = {(t, c) for t, c in true}
+#         #{('复方蜥蜴散', '方剂'), ('三七', '中药')}
+#         pred_set = {(t, c) for t, c in pred}
+#         #转成集合才能求交集
 
-# 统计 TP
-        for ent in (true_set & pred_set):#必须完全一样，否则就是空的
-            tp[ent[1]] += 1   # ent[1] 是类别
+# # 统计 TP
+#         for ent in (true_set & pred_set):#必须完全一样，否则就是空的
+#             tp[ent[1]] += 1   # ent[1] 是类别
 
-        # 统计 FP
-        for ent in (pred_set - true_set):#只保留 pred 中有，但 true 中没有的元素。pred_set - true_set如果是 {('半枝莲', '中药')}和{('一枝莲', '方剂')}则保留{('半枝莲', '中药')}
-            fp[ent[1]] += 1
-# 虽然“半枝莲”相同，但元组不同，所以：
-# 它们被视为两个不同的实体
-# 差集也会把它们区分开
-        # 统计 FN
-        for ent in (true_set - pred_set):
-            fn[ent[1]] += 1
+#         # 统计 FP
+#         for ent in (pred_set - true_set):#只保留 pred 中有，但 true 中没有的元素。pred_set - true_set如果是 {('半枝莲', '中药')}和{('一枝莲', '方剂')}则保留{('半枝莲', '中药')}
+#             fp[ent[1]] += 1
+# # 虽然“半枝莲”相同，但元组不同，所以：
+# # 它们被视为两个不同的实体
+# # 差集也会把它们区分开
+#         # 统计 FN
+#         for ent in (true_set - pred_set):
+#             fn[ent[1]] += 1
 
-    # 计算 F1
-    for cls in tp.keys() | fp.keys() | fn.keys():
-        precision = tp[cls] / (tp[cls] + fp[cls] + 1e-8)#1 × 10⁻⁸
-        recall    = tp[cls] / (tp[cls] + fn[cls] + 1e-8)
-        f1        = 2 * precision * recall / (precision + recall + 1e-8)
+#     # 计算 F1
+#     for cls in tp.keys() | fp.keys() | fn.keys():
+#         precision = tp[cls] / (tp[cls] + fp[cls] + 1e-8)#1 × 10⁻⁸
+#         recall    = tp[cls] / (tp[cls] + fn[cls] + 1e-8)
+#         f1        = 2 * precision * recall / (precision + recall + 1e-8)
 
-        print(f"类别：{cls}")
-        print(f"  Precision={precision:.4f}")
-        print(f"  Recall={recall:.4f}")
-        print(f"  F1={f1:.4f}\n")
+#         print(f"类别：{cls}")
+#         print(f"  Precision={precision:.4f}")
+#         print(f"  Recall={recall:.4f}")
+#         print(f"  F1={f1:.4f}\n")
 
         
-    #     # 每类 TP / FN / FP
-    #     for ent in true_set:
-    #         if ent in pred_set:
-    #             tp[ent[1]] += 1
-    #         else:
-    #             fn[ent[1]] += 1
+#     #     # 每类 TP / FN / FP
+#     #     for ent in true_set:
+#     #         if ent in pred_set:
+#     #             tp[ent[1]] += 1
+#     #         else:
+#     #             fn[ent[1]] += 1
 
-    #     for ent in pred_set:
-    #         if ent not in true_set:
-    #             fp[ent[1]] += 1
+#     #     for ent in pred_set:
+#     #         if ent not in true_set:
+#     #             fp[ent[1]] += 1
 
-    # # 计算每个类别 F1
-    # results = {}
-    # for cat in set(list(tp.keys()) + list(fp.keys()) + list(fn.keys())):
-    #     p = tp[cat] / (tp[cat] + fp[cat]) if tp[cat] + fp[cat] > 0 else 0
-    #     r = tp[cat] / (tp[cat] + fn[cat]) if tp[cat] + fn[cat] > 0 else 0
-    #     f1 = 2*p*r / (p+r) if p+r > 0 else 0
-    #     results[cat] = {"precision": p, "recall": r, "f1": f1}
+#     # # 计算每个类别 F1
+#     # results = {}
+#     # for cat in set(list(tp.keys()) + list(fp.keys()) + list(fn.keys())):
+#     #     p = tp[cat] / (tp[cat] + fp[cat]) if tp[cat] + fp[cat] > 0 else 0
+#     #     r = tp[cat] / (tp[cat] + fn[cat]) if tp[cat] + fn[cat] > 0 else 0
+#     #     f1 = 2*p*r / (p+r) if p+r > 0 else 0
+#     #     results[cat] = {"precision": p, "recall": r, "f1": f1}
 
-    # # 计算 overall micro F1
-    # TP = sum(tp.values())
-    # FP = sum(fp.values())
-    # FN = sum(fn.values())
-    # P = TP / (TP + FP) if TP + FP > 0 else 0
-    # R = TP / (TP + FN) if TP + FN > 0 else 0
-    # overall_f1 = 2*P*R / (P+R) if P+R > 0 else 0
+#     # # 计算 overall micro F1
+#     # TP = sum(tp.values())
+#     # FP = sum(fp.values())
+#     # FN = sum(fn.values())
+#     # P = TP / (TP + FP) if TP + FP > 0 else 0
+#     # R = TP / (TP + FN) if TP + FN > 0 else 0
+#     # overall_f1 = 2*P*R / (P+R) if P+R > 0 else 0
 
-    # return results, overall_f1
+#     # return results, overall_f1
--- infer/inference.py
+++ infer/inference.py
@@ -1,39 +1,39 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-from srctwo.data.utils import SYSTEM_prompt
-from peft import PeftModel
+# from transformers import AutoTokenizer, AutoModelForCausalLM
+# from srctwo.data.utils import SYSTEM_prompt
+# from peft import PeftModel
 
-def infer(args):
-    base_model=AutoModelForCausalLM.from_pretrained(
-        "D:\\vscode--llm\\my-llm-training\\srctwo\\models\\Qwen2.5-7B",
-        torch_dtype="auto",
-        device_map="auto"
-    )
-    model=PeftModel(base_model,"D:\\vscode--llm\\my-llm-training\\srctwo\\checkpoints\\bestmodel")
+# def infer(args):
+#     base_model=AutoModelForCausalLM.from_pretrained(
+#         "D:\\vscode--llm\\my-llm-training\\srctwo\\models\\Qwen2.5-7B",
+#         torch_dtype="auto",
+#         device_map="auto"
+#     )
+#     model=PeftModel(base_model,"D:\\vscode--llm\\my-llm-training\\srctwo\\checkpoints\\bestmodel")
 
-    tokernizer=AutoTokenizer.from_pretrained(
-        "D:\\vscode--llm\\my-llm-training\\srctwo\\models\\Qwen2.5-7B"
-    )
+#     tokernizer=AutoTokenizer.from_pretrained(
+#         "D:\\vscode--llm\\my-llm-training\\srctwo\\models\\Qwen2.5-7B"
+#     )
 
-    while True:
-        print("请输入你的问题")
-        prompt=input()
-        if prompt in ("bye","exit"):
-            break
+#     while True:
+#         print("请输入你的问题")
+#         prompt=input()
+#         if prompt in ("bye","exit"):
+#             break
         
-        message=[
-            {'role':'system','content':SYSTEM_prompt},
-            {'role':'user','content':prompt},
-        ]
-        text=tokenizer.from_chat_template(
-            message,
-            tokenize=False,
-            add_generation_prompt=True
-        )
-        models_input=tokenizer([text],return_tensors="pt").to(model.device)
-        model_output=model.generate(
-            models_input,
-            max_new_tokens=args.max_completion_length
-        )
-        generate_ids=[b[len(a):] for a,b in zip(models_input.input_ids,model_output)]
+#         message=[
+#             {'role':'system','content':SYSTEM_prompt},
+#             {'role':'user','content':prompt},
+#         ]
+#         text=tokenizer.from_chat_template(
+#             message,
+#             tokenize=False,
+#             add_generation_prompt=True
+#         )
+#         models_input=tokenizer([text],return_tensors="pt").to(model.device)
+#         model_output=model.generate(
+#             models_input,
+#             max_new_tokens=args.max_completion_length
+#         )
+#         generate_ids=[b[len(a):] for a,b in zip(models_input.input_ids,model_output)]
 
-        response=tokenizer.batch_decode(generate_ids,skip_special_tokens=True)
+#         response=tokenizer.batch_decode(generate_ids,skip_special_tokens=True)
